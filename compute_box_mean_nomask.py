21f80438729fd7edd5beb80dc2fd0f5840708807
jmecki <jmecki@noc.ac.uk>
2021-11-09 15:29:17 +0000

    updated code and removed slumr files

--- compute_box_mean_nomask.py
+++ compute_box_mean_nomask.py
@@ -1,6 +1,7 @@
 import jCMIP as jc
 import sys
 import os
+import shutil
 
 # Get information:
 inputs = sys.argv
@@ -17,11 +18,14 @@ jmin    = float(inputs[8])
 jmax    = float(inputs[9])
 outfile = inputs[10]
 lfile   = inputs[11]
+runfile = inputs[12]
 
 # No mask:
 gmask   = []
 
-jc.box_means(Model,EXP,ENS,var,vtype,imin,imax,jmin,jmax,outfile,gmask=gmask,gtype='gn')
+if not os.path.isfile(outfile):
+    jc.box_means(Model,EXP,ENS,var,vtype,imin,imax,jmin,jmax,runfile,gmask=gmask,gtype='gn')
+    shutil.move(runfile, outfile)
 
 # Delete submit file after successful computation:
 os.remove(('../' + lfile))
